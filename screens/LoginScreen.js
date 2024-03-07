2965ed7af767d74100b93aa965cf8bfff2b97938
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-03-07 11:37:24 +0530

    update: login screen layout

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,8 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import Animated,{ FadeIn, FadeInUp, FadeOut } from 'react-native-reanimated';
+
 
 export default function LoginScreen() {
   return (
@@ -18,13 +20,34 @@ export default function LoginScreen() {
       </View>
 
       {/* title and form */}
-      <View className="h-full w-full flex justify-center items-center">
-        
+      <View className="flex justify-around absolute w-full h-full pt-48">
         {/* title */}
-        <View className="flex items-center">
-        <Text className="text-black font-bold tracking-wider text-5xl">Welcome to Doniya</Text>
+        <View className="flex items-center transform -rotate-12">
+          <Text className="text-white font-bold text-5xl">
+            Doniya
+          </Text>
         </View>
 
+        {/* form */}
+        <View className="flex items-center mx-4 space-y-4">
+          <View className="bg-black/5 p-5 rounded-2xl w-full">
+            <TextInput placeholder='Email' placeholderTextColor={'gray'} />
+          </View>
+          <View className="bg-black/5 p-5 rounded-2xl w-full">
+            <TextInput placeholder='Password' placeholderTextColor={'gray'} secureTextEntry/>
+          </View>
+          <View className="w-full">
+            <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl mb-3">
+              <Text className="text-xl font-bold text-white text-center">Login</Text>
+            </TouchableOpacity>
+          </View>
+          <View className="flex-row justify-center">
+            <Text>Don't have an account? </Text>
+            <TouchableOpacity>
+              <Text className="text-sky-600">SignUp</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </View>
   )
