4c2cd64e8698385fec80e2e08dd03e0ca99238ce
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-03-09 19:18:04 +0530

    update: header file

--- components/HeaderNav.js
+++ components/HeaderNav.js
@@ -2,7 +2,7 @@ import { View, Text ,TextInput, StyleSheet} from 'react-native'
 import React from 'react'
 import { AntDesign } from '@expo/vector-icons';
 
-export default function searchBar() {
+export default function HeaderNav() {
   return (
     <View style={styles.container}>
 
